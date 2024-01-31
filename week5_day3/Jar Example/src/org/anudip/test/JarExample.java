cddf9dc5c2098da226ff7c8cb2605a32a742741a
Piyansu Saha <119123218+piyansu@users.noreply.github.com>
2024-01-31 22:09:46 +0530

    Initial commit

---
+++ week5_day3/Jar Example/src/org/anudip/test/JarExample.java
@@ -0,0 +1,11 @@
+package org.anudip.test;
+
+/**creating class named JarExample to print “Successfully executed Jar
+on successful compilation and run.
+*/
+public class JarExample{
+	public static void main(String[] args){
+		System.out.println("Successfully executed Jar");
+	}
+
+}
--- week5_day3/MavenExample.java
+++
@@ -1,9 +0,0 @@
-package week5_day3;
-
-public class MavenExample {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to Maven");
-	}
-
-}
